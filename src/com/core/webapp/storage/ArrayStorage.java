551b71beb5d3004f0e7d545937a980362d8427fe
Artemf29 <artemf29@icloud.com>
2020-11-06 00:55:25 +0300

    added method update

--- src/com/core/webapp/storage/ArrayStorage.java
+++ src/com/core/webapp/storage/ArrayStorage.java
@@ -2,24 +2,46 @@ package com.core.webapp.storage;
 
 import com.core.webapp.model.Resume;
 
+import java.util.Arrays;
+
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
         size = 0;
     }
 
-    public void save(Resume r) {
-        storage[size] = r;
+    public void save(Resume resume) {
+        if (Arrays.asList(storage).contains(resume)) {
+            System.out.println("Резюме уже добавлено!");
+            return;
+        }
+        if (size > storage.length) {
+            System.out.println("Хранилище заполнено!");
+            return;
+        }
+        if (resume == null) {
+            System.out.println("Резюме отсутствует!");
+            return;
+        }
+        storage[size] = resume;
         size++;
+    }
+
+    public void update(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(resume)) {
+                storage[i].setUuid("Update");
+                return;
+            }
+        }
+        System.out.println("Резюме не найдено!");
     }
 
     public Resume get(String uuid) {
@@ -28,22 +50,20 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
+        System.out.println("Резюме не найдено!");
         return null;
     }
 
     public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
-                while (i < size) {
-                    storage[i] = storage[i + 1];
-                    i++;
-                }
+                storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
-                break;
+                return;
             }
         }
-
+        System.out.println("Резюме не найдено!");
     }
 
     /**
@@ -51,9 +71,7 @@ public class ArrayStorage {
      */
     public Resume[] getAll() {
         Resume[] resumes = new Resume[size];
-        for (int i = 0; i < resumes.length; i++) {
-            resumes[i] = storage[i];
-        }
+        System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
     }
 
